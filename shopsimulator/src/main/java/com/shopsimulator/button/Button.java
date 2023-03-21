90c0909b93f1475fe80c2c14f0b8ebd73b807f8b
Pirdo <100035058+Pirdo@users.noreply.github.com>
2023-03-20 21:26:10 -0300

    Delete shopsimulator directory

--- shopsimulator/src/main/java/com/shopsimulator/button/Button.java
+++
@@ -1,7 +0,0 @@
-package com.shopsimulator.button;
-
-//cria a interface Button
-public interface Button {
-    //cria o metodo paint() da interface, usado para colocar algo na tela
-    void paint();
-}
--- shopsimulator/src/main/java/com/shopsimulator/checkbox/Checkbox.java
+++
@@ -1,7 +0,0 @@
-package com.shopsimulator.checkbox;
-
-//cria a interface Checkbox
-public interface Checkbox {
-    //cria o metodo paint() da interface, usado para colocar algo na tela
-    void paint();
-}
