65bf6c0e7ff735672c84a0cf26cb3c26d3b92774
Pirdo <gabrielfpires0@gmail.com>
2023-05-29 21:08:21 -0300

    bridge

---
+++ bridge/src/main/java/com/bridge/example/Demo.java
@@ -0,0 +1,28 @@
+package com.bridge.example;
+
+import com.bridge.devices.Tv;
+import com.bridge.remotes.AdvancedRemote;
+import com.bridge.remotes.BasicRemote;
+import com.bridge.devices.Device;
+import com.bridge.devices.Radio;
+
+public class Demo {
+    public static void main(String[] args) {
+        testDrive(new Tv());
+        // testDrive(new Radio());
+    }
+
+    public static void testDrive(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        advancedRemote.channelUp();
+        device.printStatus();
+    }
+}
